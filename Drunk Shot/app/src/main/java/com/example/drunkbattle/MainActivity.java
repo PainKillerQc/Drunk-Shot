6db76e1ea4608f4e4f6361555bb85ebfd6c01ed7
Ariel <afontau@outlook.com>
2019-12-04 12:48:20 -0500

    Programme qui marche pas mais ça devrait

--- Drunk Shot/app/src/main/java/com/example/drunkbattle/MainActivity.java
+++ Drunk Shot/app/src/main/java/com/example/drunkbattle/MainActivity.java
@@ -2,16 +2,29 @@ package com.example.drunkbattle;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Activity;
+import android.graphics.Canvas;
+import android.graphics.Rect;
 import android.os.Bundle;
+import android.view.Surface;
+import android.view.SurfaceHolder;
+import android.view.Window;
+import android.view.WindowManager;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        this.requestWindowFeature(Window.FEATURE_NO_TITLE);
 
-        int in = 2;
+        setContentView(new GamePanel(this));
+
+
+       // MainThread mt = new MainThread( , new GamePanel(this));
+        //mt.start();
+
 
     }
 }
---
+++ Drunk Shot/app/src/main/java/com/example/drunkbattle/RectPlayer.java
@@ -0,0 +1,15 @@
+package com.example.drunkbattle;
+
+import android.graphics.Canvas;
+
+public class RectPlayer implements GameObject {
+    @Override
+    public void draw(Canvas canvas) {
+
+    }
+
+    @Override
+    public void update() {
+
+    }
+}
