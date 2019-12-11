50b90341b96117f914b3fd9d59623d4235e58628
PainKillerQc <m.a.lauzier13@gmail.com>
2019-12-11 11:03:15 -0500

    meh

--- Drunk Shot/app/src/main/java/com/example/drunkbattle/MainActivity.java
+++ Drunk Shot/app/src/main/java/com/example/drunkbattle/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.drunkbattle;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
 import android.app.Activity;
 import android.graphics.Canvas;
 import android.graphics.Rect;
@@ -9,19 +10,14 @@ import android.os.Bundle;
 import android.util.DisplayMetrics;
 import android.view.Surface;
 import android.view.SurfaceHolder;
+import android.view.ViewGroup;
 import android.view.Window;
 import android.view.WindowManager;
+import android.widget.ImageView;
 
 public class MainActivity extends Activity {
+    
 
-    ImageView circle;
-    private ViewGroup mainLayout;
-
-    private int dx;
-    private int dy;
-
-
-    @SuppressLint("ClickableViewAccessibility")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
